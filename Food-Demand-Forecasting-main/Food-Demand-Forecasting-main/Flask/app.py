56db09a244a226078ae6f06aa83eec774ef230ac
vinu-3 <82452894+vinu-3@users.noreply.github.com>
2022-01-06 16:44:38 +0530

    Add files via upload

---
+++ Food-Demand-Forecasting-main/Food-Demand-Forecasting-main/Flask/app.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 27 08:57:55 2021
+
+@author: rincy
+"""
+
+# import the necessary packages
+import pandas as pd
+import numpy as np
+import pickle
+import os
+from flask import Flask,request, render_template
+app=Flask(__name__,template_folder="templates")
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('home.html')
+@app.route('/home', methods=['GET'])
+def about():
+    return render_template('home.html')
+@app.route('/pred',methods=['GET'])
+def page():
+    return render_template('upload.html')
+@app.route('/predict', methods=['GET', 'POST'])
+def predict():
+    print("[INFO] loading model...")
+    model = pickle.load(open('fdemand.pkl', 'rb'))
+    input_features = [float(x) for x in request.form.values()]
+    features_value = [np.array(input_features)]
+    print(features_value)
+    
+    features_name = ['homepage_featured', 'emailer_for_promotion', 'op_area', 'cuisine',
+       'city_code', 'region_code', 'category']
+    prediction = model.predict(features_value)
+    output=prediction[0]    
+    print(output)
+    return render_template('upload.html', prediction_text=output)
+
+    
+if __name__ == '__main__':
+      app.run(debug=False)
+ 
---
+++ Food-Demand-Forecasting-main/Food-Demand-Forecasting-main/Flask/ibmapp.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 27 08:57:55 2021
+
+@author: rincy
+"""
+
+# import the necessary packages
+import pandas as pd
+import numpy as np
+import pickle
+import os
+import requests
+
+# NOTE: you must manually set API_KEY below using information retrieved from your IBM Cloud account.
+API_KEY = "HdW0sTUJV2dQyaLB_voIUYGOwWkXZAMSiEmoYzZEaqOu"
+token_response = requests.post('https://iam.cloud.ibm.com/identity/token', data={"apikey": API_KEY, "grant_type": 'urn:ibm:params:oauth:grant-type:apikey'})
+mltoken = token_response.json()["access_token"]
+
+header = {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + mltoken}
+
+from flask import Flask,request, render_template
+app=Flask(__name__,template_folder="templates")
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('home.html')
+@app.route('/home', methods=['GET'])
+def about():
+    return render_template('home.html')
+@app.route('/pred',methods=['GET'])
+def page():
+    return render_template('upload.html')
+@app.route('/predict', methods=['GET', 'POST'])
+def predict():
+    print("[INFO] loading model...")
+    #model = pickle.load(open('fdemand.pkl', 'rb'))
+    input_features = [int(x) for x in request.form.values()]
+    features_value = [[np.array(input_features)]]
+    print(features_value)
+
+    payload_scoring = {"input_data":[{"field": [['homepage_featured', 'emailer_for_promotion', 'op_area', 'cuisine',
+       'city_code', 'region_code', 'category']],"values": [input_features ]}]}
+    
+    response_scoring = requests.post('https://us-south.ml.cloud.ibm.com/ml/v4/deployments/fceca4bb-5665-47f6-bb69-0d91eb60e1b4/predictions?version=2021-11-17', json=payload_scoring, headers={'Authorization': 'Bearer ' + mltoken})
+    print("Scoring response")
+    print(response_scoring.json())
+    predictions =response_scoring.json()
+    print(predictions)
+    print('Final Prediction Result',predictions['predictions'][0]['values'][0][0])
+    pred = predictions['predictions'][0]['values'][0][0]
+
+    #prediction = model.predict(features_value)
+    #output=prediction[0]    
+    #print(output)
+    print(pred)
+    return render_template('upload.html', prediction_text=pred)
+
+    
+if __name__ == '__main__':
+      app.run(debug=False)
+ 
